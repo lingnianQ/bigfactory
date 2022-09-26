2fa716c8bab9988107c44cc40bd74ea6b3419fe2
jason <qlfreesky@gmail.com>
2022-09-26 17:53:13 +0800

    git update

--- 02-spring/src/main/java/com/spring/example/Container.java
+++ 02-spring/src/main/java/com/spring/example/Container.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Lazy
-@Scope("singleton")
-@Component
+@Lazy //定义singleton对象的延迟加载特性
+@Scope("singleton") //用于定义bean作用域
+@Component //用于定义bean组件
 public class Container {
-
+    /**
+     * @PostConstruct 注解描述的方法为生命周期初始化方法
+     */
     @PostConstruct
     public void doInit(){}
-
+    /**
+     * @PreDestroy 注解描述的方法为生命周期销毁方法,单例对象销毁之前执行.
+     */
     @PreDestroy
     public void doDestory(){}
 }
--- notes/2204/Day07/Day07.md
+++ notes/2204/Day07/Day07.md
@@ -1,2 +1,9 @@
-# 思维导图
+# 思维导图及架构逻辑图
+1)MySQL
 https://www.processon.com/view/link/632d82e35653bb1ab0ea0fab
+2)MyBatis
+https://www.processon.com/view/link/632b045c07912955b2fcfc34
+https://www.processon.com/view/link/632b08a2e0b34d3300741e76
+3)Spring
+https://www.processon.com/view/link/632ed5df637689341d73101a
+https://www.processon.com/view/link/633166c5e401fd434645aad0
