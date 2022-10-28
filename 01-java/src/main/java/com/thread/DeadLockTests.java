504b1249bd0ed1103dc781453998ab4cb25d68c3
Mike <qlfreesky@gmail.com>
2022-10-28 16:29:10 +0800

    bigfactory

--- 01-java/src/main/java/com/thread/DeadLockTests.java
+++ 01-java/src/main/java/com/thread/DeadLockTests.java
@@ -3,10 +3,10 @@ package com.thread;
 /**
  * 死锁的案例演示
  */
-class SyncThread implements  Runnable{
+class SyncTask implements  Runnable{
     private Object lock1;
     private Object lock2;
-    public SyncThread(Object lock1,Object lock2){
+    public SyncTask(Object lock1,Object lock2){
         this.lock1=lock1;
         this.lock2=lock2;
     }
@@ -31,8 +31,8 @@ public class DeadLockTests {
     public static void main(String[] args) {
         Object obj1=new Object();
         Object obj2=new Object();
-        Thread t1 = new Thread(new SyncThread(obj1, obj2));
-        Thread t2 = new Thread(new SyncThread(obj2, obj1));
+        Thread t1 = new Thread(new SyncTask(obj1, obj2));
+        Thread t2 = new Thread(new SyncTask(obj2, obj1));
         t1.start();
         t2.start();
     }
