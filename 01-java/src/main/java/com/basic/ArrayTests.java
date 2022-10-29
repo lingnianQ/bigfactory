a414f7f8a2f65ba4aea4acacd70715ca4e93d161
Mike <qlfreesky@gmail.com>
2022-10-29 11:00:13 +0800

    bigfactory

---
+++ 01-java/src/main/java/com/basic/ArrayTests.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class ArrayTests {
+    public static void main(String[] args) {
+
+        int[]a1;
+        int a2[];
+       // int a3[5];//错误
+
+        int[][]b1={{}};
+        int b2[][]=new int[][]{{}};
+        int []b3[]=new int[3][4];
+        int []b4[]=new int[3][];
+        //int []b5[]=new int[][5];错误
+        //int b4[1][];//错误
+       //int b5[3][2];//错误
+
+
+        int[][][]c={{{}}};
+        int[][][][]d;
+
+
+    }
+}
